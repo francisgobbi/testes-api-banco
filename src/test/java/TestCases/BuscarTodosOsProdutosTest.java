0d078890a0ccbc42d4cfc3dc2122d1651a14fc7b
francisgobbi <francis.gobbi@db.tec.br>
2023-11-16 16:30:31 -0300

    update test buscar todos produtos com autenticacao

--- src/test/java/TestCases/BuscarTodosOsProdutosTest.java
+++ src/test/java/TestCases/BuscarTodosOsProdutosTest.java
@@ -4,19 +4,19 @@ import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import io.restassured.http.ContentType;
-import io.restassured.mapper.ObjectMapperType;
 import io.restassured.response.Response;
+import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*;
 
-public class BuscarProdutoComAutenticacao {
+@DisplayName("Testes de API Rest buscar todos produtos com autenticação")
+public class BuscarTodosOsProdutosTest {
 
     @Test
-    @DisplayName("Buscar Produto com Autenticação")
-    public void testDadoUsuarioQuandoObtenhoTokenEBuscoPorumProdutoEntaoObtenhoStatusCode200(){
-        baseURI = ("https://dummyjson.com/");
+    @DisplayName("Buscar todos produtos, apos autenticação e token")
+    public void testBuscarTodosProdutos() {
+        baseURI = ("https://dummyjson.com");
         basePath = "";
 
         String token = given()
@@ -25,21 +25,24 @@ public class BuscarProdutoComAutenticacao {
                         "    \"username\": \"kminchelle\",\n" +
                         "    \"password\": \"0lelplR\"\n" +
                         "}")
-                .when()
+        .when()
                 .post("/auth/login")
 
                 .then()
-                 .extract()
-                 .path("token");
+                    .extract()
+                        .path("token");
+
 
         given()
                 .contentType(ContentType.JSON)
-                .header("Authorization", token)
-        .when()
-                .get("/auth/products")
+                .header("Authorization",token)
 
+        .when()
+                .get("auth/products")
         .then()
-                .log().all();
+                .log().all()
+                .assertThat()
+                .statusCode(200).toString();
 
         Response response = given().contentType("application/json").get(baseURI);
         ExtentReports extent = new ExtentReports();
@@ -47,14 +50,16 @@ public class BuscarProdutoComAutenticacao {
         extent.attachReporter(spark);
 
         if (response.getStatusCode() == 200) {
-            extent.createTest("Teste buscar produto com autenticação - Status Code " + response.getStatusCode())
-                    .log(Status.PASS, "Teste buscar produto com autenticação, Passed!");
+            extent.createTest("Teste busca produtos apos autenticação - Status Code " + response.getStatusCode())
+                    .log(Status.PASS, "Teste busca produtos apos autenticação, Passed!");
             extent.flush();
         } else {
-            extent.createTest("Teste buscar apenas um produto por id - Status Code " + response.getStatusCode())
-                    .log(Status.FAIL, "Teste buscar apenas um produto por id, Fail!");
+            extent.createTest("Teste busca produtos apos autenticação - Status Code " + response.getStatusCode())
+                    .log(Status.FAIL, "Teste busca produtos apos autenticação, Fail!");
             extent.flush();
         }
     }
 
 }
+
+
